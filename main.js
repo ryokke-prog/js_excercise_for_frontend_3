80a58215daeabd24110a97039aa9e8c8a067980d
ryokke <ryotahbb@icloud.com>
2019-10-26 23:51:03 +0900

    improve main.js

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 /**
  * 課題1: id属性値が `main` のdiv要素ににul要素を追加する
  */
-
+const mainElement = document.getElementById('main');
+const ulElement = document.createElement('ul');
+mainElement.appendChild(ulElement);
 /**
  * 課題2: 課題1で作成したul要素に5つのli要素(DOM)を追加する
  *   - forループを使って5つのli要素(DOM)を追加すること
@@ -12,17 +14,25 @@
  *     4. アイテム4
  *     5. アイテム5
  */
-
+for (let i = 1; i <= 5; i++) {
+  const liElement = document.createElement('li');
+  liElement.textContent = i + '. アイテム' + i;
+  ulElement.appendChild(liElement);
+}
 
 /**
  * 課題3: 課題2で作成した5つのli要素の内、先頭から4番目(アイテム4)の要素を削除する
  *   - ヒント: DOMのchildrenプロパティが使える
  *     - https://developer.mozilla.org/ja/docs/Web/API/ParentNode/children
  */
-
+const removeLi = ulElement.children[3];
+removeLi.parentNode.removeChild(removeLi);
 
 /**
  * 課題4: id属性値が `remove-all-items` のul要素内の全てのli要素を削除する
  *   - ul要素は削除しないこと
  */
-
+const removeAllItems = document.getElementById('remove-all-items');
+while (removeAllItems.firstChild) {
+  removeAllItems.removeChild(removeAllItems.firstChild);
+}
